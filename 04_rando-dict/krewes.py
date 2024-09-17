cc34f8c7b09c153216481d7147713a36d5a4f3a2
Tahmim Hassan <thassan50@stuy.edu>
2024-09-17 10:33:33 -0400

    Did wk04 with group members, added krewes

---
+++ 04_rando-dict/krewes.py
@@ -0,0 +1,27 @@
+##Tahmim Hassan
+##SoftDev
+##K04 -- Python dictionaries and random selection
+##2024-9-13
+##time spent: 0.5
+import random
+krewes = {
+           4: [ 
+		'DUA','TAWAB','EVA','JACK','VICTOR','EVAN','JASON','COLYI','IVAN','TANZEEM',
+		'TAHMIM','STANLEY','LEON','NAOMI','NIA','ANASTASIA','JADY','BRIAN','JACOB',
+		'ALEX','CHONGTIAN','DANNY','MARCO','ABIDUR','ANKITA','ANDY','ETHAN','AMANDA',
+		'AIDAN','LINDA','QIANJUN','JIAYING','KISHI'
+		],
+           5: [ 
+                'ADITYA','MARGIE','RACHEL','ALEXANDER','ZIYAD','DANNY','ENDRIT','CADEN',
+                'VEDANT','SUHANA','KYLE','KEVIN','RAYMOND','CHRISTOPHER','JONATHAN','SASHA',
+                'NAFIYU','TIM','WILL','DANIEL','BENJAMIN','CLAIRE','CHLOE','STELLA','TRACY',
+                'JESSICA','JACKIE','WEN YUAN','YINWEI','TIFFANY','JAYDEN DANIEL','PRINCEDEN' 
+              ]
+         }
+
+def selectDevo(krewes):
+    keys = [key for key in krewes]
+    devos = krewes[keys[int(random.random()*len(krewes))]]
+    return devos[int(random.random()*len(devos))]
+
+print( selectDevo(krewes))
