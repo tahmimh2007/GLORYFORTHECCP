3ece29bba86a272f0f4fc4db2aa6d8bb19c95ca3
Tahmim Hassan <thassan50@stuy.edu>
2024-09-30 10:59:46 -0400

    work 13 done

--- 09_softserve/app.py
+++ 09_softserve/app.py
@@ -1,5 +1,5 @@
-# Tahmim Hassan
-# Fire Marshalls
+# Ivan Gontchar
+# Fire Marshalls(Jason Chao, Tahmim Hassan)
 # SoftDev
 # K09 -- Putting it Together
 # 2024-09-24
@@ -13,12 +13,13 @@ app = Flask(__name__)
 @app.route("/")                 #assign fxn to route
 def hello_world():
     print(__name__)
-    return '''
-    # Fire Marshalls-Tahmim Hassan, Ivan Gontchar, Jason Chao<br>
-    # SoftDev<br>
-    # K09 -- Putting it Together<br>
-    # 2024-09-24<br>
-    # time spent: 1 hour<br><br><b>''' + numbercruncher.maker()
+    return '''<h3>
+    Ivan Gontchar<br>
+    Fire Marshalls<br>
+    SoftDev<br>
+    K09 -- Putting it Together<br>
+    2024-09-24<br>
+    time spent: 1 hour</h3><br><br><b>''' + numbercruncher.maker()
 
 if __name__ == "__main__":      # true if this file NOT imported
     app.debug = True            # enable auto-reload upon code change
---
+++ 13_tempwork/app.py
@@ -0,0 +1,44 @@
+# Jady Lei
+# Team: 45
+# SoftDev Pd 4
+# Sep 30 2024 
+
+"""
+QCC/DISCO:
+ - in notes
+"""
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "No hablo queso!"
+
+# ~~ working code 
+import csv
+import random
+
+with open("data/occupations.csv", newline="") as csvfile:
+    #creates a dictionary for every row that can be parsed through
+    reader = csv.DictReader(csvfile)
+    jobs = []
+    percents = []
+    full = []
+    for row in reader:
+        jobs.append(row['Job Class']), percents.append(float(row['Percentage']))
+        full.append({"Job Class": row['Job Class'], "Percentage": row['Percentage']})
+
+def ReturnRandom():
+# random.choices returns a list, [:-1] to ignore last row, k is returned list size
+    return (random.choices(jobs[:-1], weights=percents[:-1], k=1)[0])
+
+# ~~ back to app stuff
+
+@app.route("/wdywtbwygp") 
+def test_tmplt():
+    return render_template( 'tablified.html', foo= ReturnRandom(), coll = full)
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
