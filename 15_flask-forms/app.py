ff2909bfe68dc48cc7602636052b5c628811f2f2
Tahmim Hassan <thassan50@stuy.edu>
2024-10-09 10:06:23 -0400

    added work from 15

--- 15_flask-forms/app.py
+++ 15_flask-forms/app.py
@@ -2,88 +2,44 @@
 # UWSD
 # SoftDev
 # K15 -- Take and Give
-# 2024-10-8
-# time spent: 0.5
+# 2024-10-08
 
-# import conventions:
-# list most general first (standard python library)
-# ...then pip installs (eg Flask)
-# ...then your own home-rolled modules/packages (today's test module)
-
-from flask import Flask             #facilitate flask webserving
-from flask import render_template   #facilitate jinja templating
-from flask import request           #facilitate form submission
-
-#the conventional way:
 from flask import Flask, render_template, request
 
-app = Flask(__name__)    #create Flask object
+app = Flask(__name__)    # Create Flask object
 
+header = '''Team Name: UWSD
+Roster: Stanley Hoo, Tahmim Hassan, Leon Huang'''
 
-html_website = '''
-<!DOCTYPE html>
-    <head>
-        <title>
-            TITLE
-        </title>
-        <style>
-        STYLE
-        </style>
-        HEADER
-    </head>
-    <body>
-        BODY
-    </body>
-</html>'''
-
+# Landing page
 @app.route("/", methods=['GET', 'POST'])
 def disp_loginpage():
-    print("\n\n\n")
-    print("***DIAG: this Flask obj ***")
+    print("***DIAG: Flask object ***")
     print(app)
-    print("***DIAG: request obj ***")
+    print("***DIAG: Request object ***")
     print(request)
-    print("***DIAG: request.args ***")
+    print("***DIAG: Request arguments ***")
     print(request.args)
-#    print("***DIAG: request.args['username']  ***")
-#    print(request.args['username'])
-#    print("***DIAG: request.headers ***")
-#    print(request.headers)
-    return render_template( 'login.html' )
+    return render_template('login.html', header=header)
 
-
+# Response page
 @app.route("/auth", methods=['GET', 'POST'])
 def authenticate():
-#     print("\n\n\n")
-#     print("***DIAG: this Flask obj ***")
-#     print(app)
-    print("***DIAG: request obj ***")
-#     print(request)
-#     print("***DIAG: request.args ***")
-    print(request.args)
-    print("***DIAG: request.args['username']  ***")
-    print(request.args['username'])
-#     print("***DIAG: request.headers ***")
-#     print(request.headers)
-    return "Waaaa hooo HAAAH"  #response to a form submission
+    # Capture user input and request method
+    username = request.form.get("username") if request.method == "POST" else request.args.get("username")
+    method_used = request.method
+    greeting = f"Hello, {username}!" if username else "Hello!"
 
-def htmlOut(template):
-    template = template.replace("HEADER", "<h1></h1>\n")
-    template = template.replace("TITLE", "Template for Success\n")
-    body = ''
-    body += "<p>Period 4</p>\n"
-    random_job, data = choose_random('data/occupations.csv')
-    body += f"<p>{random_job}</p>\n"
-    body += make_table([key for key in data.keys()], data)
-    template = template.replace("BODY", body)
-    html_file = open("templates/response.html", "w")
-    print(template, file=html_file)
-    html_file.close()
-    return template
+    return render_template(
+        'response.html',
+        username=username,
+        method_used=method_used,
+        greeting=greeting,
+        header=header
+    )
 
-    
-if __name__ == "__main__": #false if this file imported as module
-    #enable debugging, auto-restarting of server when this file is modified
-    app.debug = True 
+if __name__ == "__main__":
+    app.debug = True
     app.run()
+
 
