98b2073dc0319558e2ab93deefb4da1cc5fd2b51
Tahmim Hassan <thassan50@stuy.edu>
2024-10-15 10:34:05 -0400

    work

--- 16_flask-sessions/app.py
+++ 16_flask-sessions/app.py
@@ -3,70 +3,63 @@
 # SoftDev
 # K16 -- Take and Keep
 # 2024-10-10
-# Time spent: 1.5h
+# Time spent: 2.5h
 
-from flask import Flask, request, render_template, redirect, url_for, flash, session
+from flask import Flask, request, render_template, make_response, redirect, url_for
 import os
-
 
 app = Flask(__name__)
 
-secret_key = os.urandom(32)
-app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+# Set secret key for session management
+app.secret_key = os.urandom(32)
 
+# Team Header
 header = '''Team Name: UWSD
 Roster: Stanley Hoo, Tahmim Hassan, Leon Huang'''
 
 @app.route('/')
 def index():
-    if 'username' in session:
-        return f'Logged in as {session["username"]}'
-#     return 'You are not logged in'
+    username = request.cookies.get('username')
+    if username:
+        return render_template('response.html', username=username, greeting=f"Hello, {username}!", header=header, method_used=request.method)
     return render_template('login.html', header=header)
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route("/response", methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        password = request.form['password']
-        if password==app.secret_key:
-            flash('Login successful!', 'success')
-            session['username'] = request.form['username']
-            password = request.form['password']
-            return redirect(url_for('/'))
-        else:
-            flash('Incorrect username or password', 'success')
-    return render_template('login.html', header=header)
+        username = request.form['username']
+        greeting = f"Hello, {username}!" if username else "Hello!"
+        response = make_response(render_template('response.html', username=username, request="POST", greeting=greeting, header=header))
+        response.set_cookie('username', username)
+        return response
+    elif request.method == 'GET':
+        username = request.args.get('username')
+        if username:
+            greeting = f"Hello, {username}!"
+            response = make_response(render_template('response.html', username=username, request="GET", greeting=greeting, header=header))
+            response.set_cookie('username', username)
+            return response
+        return redirect(url_for('index'))
 
-@app.route("/auth", methods=['GET', 'POST'])
-def authenticate():
-    # Capture user input and request method
-    username = ""
-    if request.method == 'POST':
-        password = request.args['password']
-        if password==app.secret_key:
-            flash('Login successful!', 'success')
-            username = request.args['username']
-            session['username'] = username
-            password = request.args['password']
-            return redirect(url_for('/'))
-        else:
-            flash('Incorrect username or password', 'success')
-    method_used = request.method
-    greeting = f"Hello, {username}!" if username else "Hello!"
-    return render_template(
-        'response.html',
-        username=username,
-        method_used=method_used,
-        greeting=greeting,
-        header=header
-    )
-
+@app.route('/auth', methods=['GET'])
+def auth():
+    username = request.args.get('username')
+    password = request.args.get('password')
+    if username and password:
+        greeting = f"Hello, {username}!"
+        response = make_response(render_template('response.html', username=username, request="GET", greeting=greeting, header=header))
+        response.set_cookie('username', username)
+        return response
+    return redirect(url_for('index'))
 
 @app.route('/logout')
 def logout():
-    # remove the username from the session if it's there
-    session.pop('username', None)
-    return redirect(url_for('index'))
+    username = request.cookies.get('username')
+    if not username:
+        return redirect(url_for('index'))
+    logout_response = make_response(render_template('logout.html', header=header, username=username))
+    logout_response.delete_cookie('username')
+    return logout_response
 
 if __name__ == "__main__":
     app.debug = True
